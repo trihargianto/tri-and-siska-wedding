13a8ca7c7a2fbd290053fd0161f1026bedd37263
Tri Hargianto <trihargianto@gmail.com>
2023-06-10 01:55:27 +0700

    install fonts, sass, and remedy.css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/google-fonts"],
+  css: ["~/assets/css/remedy.css", "~/assets/css/global.css"],
+  googleFonts: {
+    families: {
+      Inter: true,
+    },
+  },
+});
