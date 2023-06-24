097fb3417951153b94a3cf2c96ff2aaecec3acc9
Tri Hargianto <trihargianto@gmail.com>
2023-06-24 14:51:46 +0700

    add meta tag & update favicon (#9)
    * add meta tag & update favicon
    
    * add og:image width & height
    
    * update image meta tag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,73 @@
 const routerBase = process.env.APP_ROUTER_BASE;
+const domain = "https://tri-siska.fyi";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
     baseURL: routerBase,
+    head: {
+      title: "Tri & Siska",
+      meta: [
+        {
+          name: "robots",
+          content: "noindex, nofollow",
+        },
+        {
+          name: "description",
+          content: "Tri & Siska Wedding Invitation - 30 Juli 2023",
+        },
+        // Facebook / Open Graph
+        {
+          property: "og:type",
+          content: "website",
+        },
+        {
+          property: "og:url",
+          content: domain,
+        },
+        {
+          property: "og:title",
+          content: "Tri & Siska",
+        },
+        {
+          property: "og:description",
+          content: "Tri & Siska Wedding Invitation - 30 Juli 2023",
+        },
+        {
+          property: "og:image:width",
+          content: "500",
+        },
+        {
+          property: "og:image:height",
+          content: "500",
+        },
+        {
+          property: "og:image",
+          content: "/tri-siska-banner.jpg",
+        },
+        // Twitter
+        {
+          name: "twitter:card",
+          content: "summary_large_image",
+        },
+        {
+          name: "twitter:url",
+          content: domain,
+        },
+        {
+          name: "twitter:title",
+          content: "Tri & Siska",
+        },
+        {
+          name: "twitter:description",
+          content: "Tri & Siska Wedding Invitation - 30 Juli 2023",
+        },
+        {
+          name: "twitter:image",
+          content: "/tri-siska-banner.jpg",
+        },
+      ],
+    },
   },
   ssr: true,
   devtools: { enabled: true },
